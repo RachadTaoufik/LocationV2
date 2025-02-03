bbb9bc19a976937614035d3c3710166d67e1c4b6
Taoufik Rachad <rachad.taoufik@gmail.com>
2025-02-03 17:07:54 +0100

    Initial commit

---
+++ app/src/main/java/com/example/location/activities/Authentification.java
@@ -0,0 +1,58 @@
+package com.example.location.activities;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.location.R;
+
+public class Authentification extends AppCompatActivity implements DialogInterface.OnClickListener {
+
+    LinearLayout layout1;
+    LinearLayout layout2;
+    Button bttauth;
+    Button bttacc;
+    EditText login;
+    EditText passwod;
+    EditText nom;
+    EditText prenom;
+    EditText pays;
+    EditText adresse;
+    EditText phone;
+    EditText email;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_authentification);
+
+        layout1=(LinearLayout) findViewById(R.id.Layout1);
+        layout2=(LinearLayout)findViewById(R.id.Layout2);
+        bttauth=(Button) findViewById(R.id.authButton);
+        bttauth.setOnClickListener(this);
+        //....
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    public void onClick(DialogInterface dialogInterface, int i) {
+
+    }
+}
